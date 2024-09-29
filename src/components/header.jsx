55503edf2b0a7a1029966b46a73d5e1403c0e35f
Cam Jackson <camjacko86@gmail.com>
2024-09-29 17:39:46 +1000

    Added contact details

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,15 +8,28 @@ export const Header = (props) => {
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
-                  {props.data ? props.data.title : "Loading"}
+                <h1 style={{ color: "black", WebkitTextStroke: "2px white" }}>
+                  {props.data ? (
+                    <>
+                      {props.data.title.split("5")[0]}
+                      <span
+                        style={{
+                          color: "black",
+                          WebkitTextStroke: "1px white",
+                          color: "#FF1493",
+                        }}
+                      >
+                        5
+                      </span>
+                      {props.data.title.split("5")[1]}
+                    </>
+                  ) : (
+                    "Loading"
+                  )}
                   <span></span>
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a
-                  href="#features"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
+                <a href="#about" className="btn btn-custom btn-lg page-scroll">
                   Learn More
                 </a>{" "}
               </div>
